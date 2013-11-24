51b2504912e07872c36d4eb40c8f80f1f5883c56
Lukasz <lukaszpakula.pl@gmail.com>
2013-11-24 10:29:05 +0000

    add C library to docs/conf

--- docs/conf.py
+++ docs/conf.py
@@ -268,7 +268,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['libcurl4-gnutls-dev']
+MOCK_MODULES = ['libcurl4-gnutls-dev', 'librtmp-dev']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
