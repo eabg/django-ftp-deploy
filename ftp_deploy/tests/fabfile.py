db9fe2f36763be32fd2fa15e1cc349e00811d26f
Lukasz Pakula <lukaszpakula.pl@gmail.com>
2014-01-12 11:20:00 +0000

    update test fabfile

--- ftp_deploy/tests/fabfile.py
+++ ftp_deploy/tests/fabfile.py
@@ -11,16 +11,17 @@ def testc():
     local("python ../../manage.py test ftp_deploy --settings=ftp_deploy.tests.conf.confc --exe")
 
 
-def testu():
+def testu(module=''):
     """Unit Tests"""
-    local("python ../../manage.py test ftp_deploy --exclude=integration_tests --exclude==external_tests --settings=ftp_deploy.tests.conf.conf --exe")
+    if module:
+        module = '.tests.unit_tests.%s' % module
+
+    local("python ../../manage.py test ftp_deploy%s --exclude=integration_tests --exclude==external_tests --settings=ftp_deploy.tests.conf.conf --exe" % module)
 
 
-def testi():
+def testi(module=''):
     """Integration Tests"""
-    local("python ../../manage.py test ftp_deploy --exclude=external_tests --exclude=unit_tests  --settings=ftp_deploy.tests.conf.conf --exe")
+    if module:
+        module = '.tests.integration_tests.%s' % module
 
-
-def teste():
-    """External Tests (FTP, repository etc.)"""
-    local("python ../../manage.py test ftp_deploy --exclude=unit_tests --exclude=integration_tests --settings=ftp_deploy.tests.conf.conf --exe")
+    local("python ../../manage.py test ftp_deploy%s --exclude=external_tests --exclude=unit_tests  --settings=ftp_deploy.tests.conf.conf --exe" % module)
