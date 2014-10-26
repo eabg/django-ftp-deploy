40ba139b15bf0936b06fe40565ca6e5b1e8c26a1
Lukasz Pakula <lukaszpakula.pl@gmail.com>
2014-10-26 11:42:14 +0000

    PEP8 update

--- ftp_deploy/tests/fabfile.py
+++ ftp_deploy/tests/fabfile.py
@@ -1,14 +1,16 @@
-from fabric.api import local, lcd, run, env
+from fabric.api import local
 
 
 def test():
     """All Tests"""
-    local("python ../../manage.py test ftp_deploy --settings=ftp_deploy.tests.conf.conf --exe")
+    local("python ../../../../manage.py test ftp_deploy \
+          --settings=ftp_deploy.tests.conf.conf --exe")
 
 
 def testc():
     """All Tests with coverage"""
-    local("python ../../manage.py test ftp_deploy --settings=ftp_deploy.tests.conf.confc --exe")
+    local("python ../../../../manage.py test ftp_deploy \
+          --settings=ftp_deploy.tests.conf.confc --exe")
 
 
 def testu(module=''):
@@ -16,7 +18,9 @@ def testu(module=''):
     if module:
         module = '.tests.unit_tests.%s' % module
 
-    local("python ../../manage.py test ftp_deploy%s --exclude=integration_tests --exclude==external_tests --settings=ftp_deploy.tests.conf.conf --exe" % module)
+    local("python ../../../../manage.py test ftp_deploy%s \
+          --exclude=integration_tests --exclude==external_tests \
+          --settings=ftpdeploy.conf --exe" % module)
 
 
 def testi(module=''):
@@ -24,4 +28,6 @@ def testi(module=''):
     if module:
         module = '.tests.integration_tests.%s' % module
 
-    local("python ../../manage.py test ftp_deploy%s --exclude=external_tests --exclude=unit_tests  --settings=ftp_deploy.tests.conf.conf --exe" % module)
+    local("python ../../../../manage.py test ftp_deploy%s \
+          --exclude=external_tests --exclude=unit_tests  \
+          --settings=ftpdeploy.conf --exe" % module)
