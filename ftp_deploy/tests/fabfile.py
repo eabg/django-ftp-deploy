6a993f275b4201b2e22caec8cb37a5bb5fa26acf
Lukasz Pakula <lukaszpakula.pl@gmail.com>
2014-10-28 17:34:06 +0000

    Updatefabfile to use django-admin.py

--- ftp_deploy/tests/fabfile.py
+++ ftp_deploy/tests/fabfile.py
@@ -1,18 +1,16 @@
-import settings
-
 from fabric.api import local
 
 
 def test():
     """All Tests"""
-    local("python ../manage.py test ftp_deploy \
-          --settings=%s.conf --exe", settings.PROJECT_NAME)
+    local("django-admin.py test ftp_deploy \
+          --settings=ftp_deploy.tests.conf.conf --exe")
 
 
 def testc():
     """All Tests with coverage"""
-    local("python ../manage.py test ftp_deploy \
-          --settings=%s.confc --exe", settings.PROJECT_NAME)
+    local("django-admin.py test ftp_deploy \
+          --settings=ftp_deploy.tests.conf.confc --exe")
 
 
 def testu(module=''):
@@ -20,9 +18,9 @@ def testu(module=''):
     if module:
         module = '.tests.unit_tests.%s' % module
 
-    local("python ../manage.py test ftp_deploy%s \
+    local("django-admin.py test ftp_deploy%s \
           --exclude=integration_tests --exclude==external_tests \
-          --settings=%s.conf.conf --exe" % (module, settings.PROJECT_NAME))
+          --settings=ftp_deploy.tests.conf.conf --exe" % module)
 
 
 def testi(module=''):
@@ -30,6 +28,6 @@ def testi(module=''):
     if module:
         module = '.tests.integration_tests.%s' % module
 
-    local("python ../manage.py test ftp_deploy%s \
+    local("django-admin.py test ftp_deploy%s \
           --exclude=external_tests --exclude=unit_tests  \
-          --settings=%s.conf.conf --exe" % (module, settings.PROJECT_NAME))
+          --settings=ftp_deploy.tests.conf.conf --exe" % module)
