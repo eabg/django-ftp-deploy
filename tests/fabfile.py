e2bea8f9e86ff0a6953e4c81ce897bc280012654
Lukasz Pakula <lukaszpakula.pl@gmail.com>
2014-10-31 08:11:19 +0000

    Amend fabfile

--- tests/fabfile.py
+++ tests/fabfile.py
@@ -16,25 +16,24 @@ def testc():
 def testu(module=''):
     """Unit Tests"""
     if module:
-        module = '.tests.unit_tests.%s' % module
+        module = '.unit_tests.%s' % module
 
     local("django-admin.py test tests%s \
-          --exclude=integration_tests --exclude==external_tests \
+          --exclude='integration_tests|external_tests' \
           --settings=tests.conf.conf --exe" % module)
 
 
 def testi(module=''):
     """Integration Tests"""
     if module:
-        module = '.tests.integration_tests.%s' % module
+        module = '.integration_tests.%s' % module
 
     local("django-admin.py test tests%s \
-          --exclude=external_tests --exclude=unit_tests  \
-          --settings=tests.conf.conf --exe" % module)
+          --exclude='unit_tests' --settings=tests.conf.conf --exe" % module)
 
 
 def testci():
     """Travis CI Tests"""
     local("django-admin.py test tests \
-          --exclude=integration_tests --exclude==external_tests \
+          --exclude='external_tests' \
           --settings=tests.conf.conf_travis --exe")
