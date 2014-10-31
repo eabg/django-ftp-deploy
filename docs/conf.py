8760bd0042870c10eedacff84a78ce80e3f2eede
Lukasz Pakula <lukaszpakula.pl@gmail.com>
2014-10-31 19:51:04 +0000

    Update release version

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ copyright = u'2013, Lukasz Pakula'
 # built documents.
 #
 # The short X.Y version.
-version = '1.4.0'
+version = '1.4.1'
 # The full version, including alpha/beta/rc tags.
 release = '1.4'
 
--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='django-ftp-deploy',
-    version='1.4.0',
+    version='1.4.1',
     author='Lukasz Pakula',
     author_email='lukaszpakula.pl@gmail.com',
     packages=[
-        'ftp_deploy', 
+        'ftp_deploy',
         'ftp_deploy.migrations',
         'ftp_deploy.tests',
         'ftp_deploy.models',
@@ -22,7 +22,6 @@ setup(
     description='Auto FTP deployment for django.',
     long_description=open('README.rst').read(),
     install_requires=[
-        "Django >= 1.5",
         "pycurl == 7.19.0.1",
         "certifi == 0.0.8",
         "django-braces == 1.2.1",
